346febfeb01b8df11c40f80f2c0a4b39b0deccae
Suba177 <mikhail.sard@mail.ru>
2024-04-29 16:30:14 +0700

    working code

--- src/pages/Blogpage.jsx
+++ src/pages/Blogpage.jsx
@@ -1,8 +1,20 @@
 import { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useSearchParams } from "react-router-dom";
 
 export default function Blogpage() {
   const [posts, setPosts] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const postQuery = searchParams.get("post") || "";
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+
+    const query = form.search.value;
+
+    setSearchParams({ post: query });
+  };
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
@@ -13,11 +25,17 @@ export default function Blogpage() {
   return (
     <div>
       <h1>Our news</h1>
-      {posts.map((post) => (
-        <Link key={post.id} to={`/posts/${post.id}`}>
-          <li>{post.title}</li>
-        </Link>
-      ))}
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <input type="search" name="search" />
+        <input type="submit" value="Search" />
+      </form>
+      {posts
+        .filter((post) => post.title.includes(postQuery) && post.id <= 20)
+        .map((post) => (
+          <Link key={post.id} to={`/posts/${post.id}`}>
+            <li>{post.title}</li>
+          </Link>
+        ))}
     </div>
   );
 }
